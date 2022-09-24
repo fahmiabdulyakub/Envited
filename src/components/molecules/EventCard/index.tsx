9aec8d07da1ce6cdf5db68d54ed58d2dce153101
fahmi.yakub.flip <fahmi.yakub@flip.id>
2022-09-24 11:02:34 +0700

    create event card

---
+++ src/components/molecules/EventCard/index.tsx
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import styles from './styles';
+
+const EventCard = () => {
+  return (
+    <View style={styles.container}>
+      <Text>EventCard</Text>
+    </View>
+  );
+};
+
+export default EventCard;
