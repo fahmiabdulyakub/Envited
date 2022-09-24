994b49fa0b5366ca3c44b6b1f8323abefdf5038a
fahmi.yakub.flip <fahmi.yakub@flip.id>
2022-09-24 08:56:21 +0700

    add title and description

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,22 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import styles from './styles';
+import {GradientText} from 'components';
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <View style={styles.containerContent}>
+        <Text style={styles.titleDark}>Imagine if</Text>
+        <GradientText style={[styles.title, styles.bottom]}>
+          Snapchat
+        </GradientText>
+        <Text style={[styles.titleDark, styles.bottom20]}>had events.</Text>
+        <Text style={styles.description}>
+          Easily host and share events with your friends across any social
+          media.
+        </Text>
+      </View>
     </View>
   );
 };
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,8 +1,37 @@
 import {StyleSheet} from 'react-native';
+import {Colors, Fonts} from 'themes';
+import {hp, wp} from 'utils/StyleUtil';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: Colors.secondary3,
+  },
+  title: {
+    fontFamily: Fonts.HelveticaBold,
+    fontSize: hp(3.5),
+  },
+  titleDark: {
+    fontFamily: Fonts.HelveticaBold,
+    fontSize: hp(3.5),
+    color: Colors.primary,
+  },
+  containerContent: {
+    alignItems: 'center',
+    marginTop: hp(7),
+    paddingHorizontal: wp(5),
+  },
+  bottom: {
+    bottom: hp(1.2),
+  },
+  bottom20: {
+    bottom: 20,
+  },
+  description: {
+    fontFamily: Fonts.HelveticaLight,
+    fontSize: hp(2),
+    color: Colors.neutral,
+    textAlign: 'center',
   },
 });
 
